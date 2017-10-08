2ac81b9f8cba9ac25420daeb786fd2aead5f303c
Phillis <Phillis@wifi-roaming-128-4-220-166.host.udel.edu>
2017-10-08 15:55:18 -0400

    Final Commit

--- PS3/src/test/java/pkgCore/DeckTest.java
+++ PS3/src/test/java/pkgCore/DeckTest.java
@@ -4,29 +4,62 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import pkgEnum.*;
+import pkgException.DeckException;
+
 public class DeckTest {
 
-	@Test
-	public void TestEmptyDeck() {
-		//TODO: Build a deck, draw until you get a DeckException
-	}
-	
-	@Test
-	public void TestDrawSuit() {
-		//TODO: Build a deck, test the Draw(eSuit) method
-	}
-	
-	@Test
-	public void TestDrawRank() {
-		//TODO: Build a deck, test the Draw(eRank) method
-	}
-	
-	public void TestDeckRankCount() {
-		//TODO: Build a deck, test the DeckRankCount method
-	}
-	
-	public void TestDeckSuitCount() {
-		//TODO: Build a deck, test the DeckSuitCount method
+	@Test(expected = DeckException.class)
+	public void TestEmptyDeck() throws DeckException {
+		Deck d = new Deck();
+		for (int i = 1; i < 54; i++) {
+			d.Draw();
+		}
 	}
 
+	@Test
+	public void TestDrawSuit() {
+		Deck d = new Deck();
+
+		for (eSuit ieSuit : eSuit.values()) {
+			Card c = d.Draw(ieSuit);
+			assertTrue(c.geteSuit() == ieSuit);
+		}
+	}
+
+	@Test
+	public void TestDrawRank() {
+		Deck d = new Deck();
+		for (eRank ieRank : eRank.values()) {
+			Card c = d.Draw(ieRank);
+			assertTrue(c.geteRank() == ieRank);
+		}
+	}
+
+	@Test
+	public void TestDeckRankCount() {
+		Deck d = new Deck();
+		for (eRank ieRank : eRank.values()) {
+			int rankCount = d.cardCounteRank(ieRank);
+			assertEquals(rankCount, 4);
+		}
+	}
+
+	@Test
+	public void TestDeckSuitCount() {
+		Deck d = new Deck();
+		for (eSuit ieSuit : eSuit.values()) {
+			int suitCount = d.cardCounteSuit(ieSuit);
+			assertEquals(suitCount, 13);
+		}
+	}
+
+	@Test
+	public void TestIsCardLeft() throws DeckException {
+		Deck d = new Deck();
+		for (int i = 1; i < 53; i++) {
+			Card c = d.Draw();
+			assertEquals(d.isCardLeft(c.geteSuit(), c.geteRank()), 0);
+		}
+	}
 }
